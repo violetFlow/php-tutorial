a35e88286d114cc45151a02aec9b63d2833f3a32
Junha park <grd0716@gmail.com>
2024-09-01 22:50:14 +0900

    php syntax

---
+++ 01-php-syntax.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+    <?php
+    echo "Hello World!";
+    $color = "red";
+    echo "My car is " . $color . "<br>"; 
+    echo "My house is " . $color . "<br>"; 
+    echo "My boat is " . $color . "<br>"; 
+    ?>
+</body>
+</html>
